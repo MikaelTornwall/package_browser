89afdcef38e2b73cedbc5115241c8a81a18e4958
Mikael Törnwall <mikael.tornwall@aalto.fi>
2020-03-13 16:26:44 +0200

    update port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ packageService.initPackages()
 app.use(cors())
 app.use('/api/packages', packageRouter)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
